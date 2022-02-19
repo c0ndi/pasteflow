b159738b2fabf8bd8aae705d434e81b54829f37d
c0ndi <73940688+c0ndi@users.noreply.github.com>
2022-02-19 11:45:38 +0100

    Setup Firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { MantineProvider } from "@mantine/core";
+import { createContext, useContext, useState } from "react";
+import "../styles/globals.css";
+
+const AuthContext = createContext();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	const [user, setUser] = useState();
+
+	return (
+		<AuthContext.Provider value={{ user, setUser }}>
+			<MantineProvider>
+				<Component {...pageProps} />
+			</MantineProvider>
+		</AuthContext.Provider>
+	);
 }
 
-export default MyApp
+export default MyApp;
+
+export const useAuth = () => {
+	return useContext(AuthContext);
+};
