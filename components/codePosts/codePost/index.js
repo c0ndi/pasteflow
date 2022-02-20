7dae215ade8e9752f134e5358e0270fe15b990d5
c0ndi <73940688+c0ndi@users.noreply.github.com>
2022-02-20 13:27:39 +0100

    Add dynamic routing

--- components/codePosts/codePost/index.js
+++ components/codePosts/codePost/index.js
@@ -1,5 +1,21 @@
-function CodePost({author, date, authorId}) {
-    return (  );
+import { Prism } from "@mantine/prism";
+import { Box } from "@mantine/core";
+import Link from "next/link";
+
+function CodePost({ author, date, postData, authorId }) {
+	return (
+		<Box sx={{ padding: "2em 0", color: "#ced4da" }}>
+			<Link href={`/${authorId}`}>
+				<Box sx={{ display: "flex", justifyContent: "space-between", cursor: "pointer" }}>
+					<p>{author}</p>
+					<p>{new Date(date).toLocaleDateString()}</p>
+				</Box>
+			</Link>
+			<Prism language="js" colorScheme="dark" withLineNumbers>
+				{postData}
+			</Prism>
+		</Box>
+	);
 }
 
 export default CodePost;
--- components/codePosts/previewCodePost/index.js
+++ components/codePosts/previewCodePost/index.js
@@ -1,5 +1,34 @@
-function PreviewCodePost() {
-    return (  );
+import { onValue, ref } from "firebase/database";
+import { useState, useEffect } from "react";
+import { db } from "../../../config";
+import CodePost from "../codePost";
+
+function PreviewCodePost({ authorId }) {
+	const [codePosts, setCodePosts] = useState([]);
+
+	useEffect(() => {
+		onValue(ref(db, "posts/" + authorId), (snapshot) => {
+			if (snapshot.val()) {
+				const allPosts = [];
+				Object.values(snapshot.val()).forEach((post) => {
+					allPosts.push(post);
+				});
+
+				allPosts.sort((a, b) => a.date - b.date);
+				setCodePosts(allPosts.reverse());
+			}
+		});
+
+		return () => setCodePosts();
+	}, []);
+
+	return (
+		<div>
+			{codePosts.map(({ author, date, postData, authorId }) => (
+				<CodePost author={author} date={date} postData={postData} authorId={authorId} />
+			))}
+		</div>
+	);
 }
 
 export default PreviewCodePost;
