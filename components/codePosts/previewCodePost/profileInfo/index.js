51597c4dfefd03684dec75f8be4c850b620d5ce9
c0ndi <73940688+c0ndi@users.noreply.github.com>
2022-02-27 17:45:53 +0100

    Update index.js

--- components/codePosts/previewCodePost/profileInfo/index.js
+++ components/codePosts/previewCodePost/profileInfo/index.js
@@ -3,22 +3,27 @@ import { onValue, push, ref, set, update } from "firebase/database";
 import { useEffect, useState } from "react";
 import { db } from "../../../../config";
 import { useAuth } from "../../../../pages/_app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 function ProfileInfo({ authorId }) {
+	const Router = useRouter();
 	const { user } = useAuth();
 	const [userData, setUserData] = useState({
 		username: "",
+		following: [],
+		followers: [],
 	});
-	const [following, setFollowing] = useState([]);
 
 	useEffect(() => {
 		onValue(ref(db, `/users/${authorId}`), (snapshot) => {
 			setUserData({
 				username: snapshot.val().username,
+				following: snapshot.val().following,
+				followers: snapshot.val().followers,
 			});
 		});
-	}, []);
-
+	}, [authorId]);
 	function followUser() {
 		onValue(
 			ref(db, `/users/${authorId}`),
@@ -56,15 +61,15 @@ function ProfileInfo({ authorId }) {
 						userId: snapshot.val().userId,
 					});
 				}
-				setFollowing(allFollowing);
 			},
 			{
 				onlyOnce: true,
 			},
 		);
 	}
+
 	return (
-		<Box sx={{ display: "flex", alignItems: "center", position: "relative", width: "100%" }}>
+		<Box sx={{ display: "flex", alignItems: "center", position: "relative", width: "100%", marginBottom: "10%" }}>
 			<Box
 				sx={{
 					overflow: "hidden",
@@ -79,11 +84,29 @@ function ProfileInfo({ authorId }) {
 			</Box>
 			<Text sx={{ fontSize: "2.5rem", padding: "0 2rem", color: "#c2c2c2" }}>@&nbsp;{userData.username}</Text>
 			{authorId !== user.uid &&
-				(following.includes(user.uid) ? (
+				(userData.followers.includes(user.uid) ? (
 					<Button disabled>You already following</Button>
 				) : (
 					<Button onClick={followUser}>Follow user</Button>
 				))}
+			<Box sx={{ position: "absolute", right: "10%", top: "5%" }}>
+				<Text sx={{ color: "#fff" }}>
+					<Text sx={{ fontWeight: "bold" }}>Followers:</Text> <br></br>{" "}
+					{userData.followers.map((follower) => (
+						<Link href={`/${follower}`}>
+							<Text sx={{ cursor: "pointer" }}>{follower}</Text>
+						</Link>
+					))}
+				</Text>
+				<Text sx={{ color: "#fff" }}>
+					<Text sx={{ fontWeight: "bold" }}>Following:</Text> &nbsp;
+					{userData.following.map((follower) => (
+						<Link href={`/${follower}`}>
+							<Text sx={{ cursor: "pointer" }}>{follower}</Text>
+						</Link>
+					))}
+				</Text>
+			</Box>
 		</Box>
 	);
 }
